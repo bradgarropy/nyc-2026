9e541bf1f0fafd36eb7229c8cbc90f9566f09d91
Brad Garropy <bradgarropy@gmail.com>
2026-06-28 23:49:20 -0500

    Clarify NYC day route segments

--- src/data/days.ts
+++ src/data/days.ts
@@ -27,15 +27,25 @@ export const days: Day[] = [
             "hotel",
         ],
         segments: [
+            // Lincoln Tunnel -> Macy's
             {mode: "car"},
+            // Macy's -> Times Square
             {mode: "walk"},
+            // Times Square -> Disney Store
             {mode: "walk"},
+            // Disney Store -> M&M's Store
             {mode: "walk"},
+            // M&M's Store -> Rockefeller Center
             {mode: "walk"},
+            // Rockefeller Center -> Nintendo Store
             {mode: "walk"},
+            // Nintendo Store -> St. Patrick's Cathedral
             {mode: "walk"},
+            // St. Patrick's Cathedral -> LEGO Store
             {mode: "walk"},
+            // LEGO Store -> John's of Times Square
             {mode: "walk"},
+            // John's of Times Square -> Hotel
             {mode: "walk"},
         ],
     },
@@ -60,12 +70,19 @@ export const days: Day[] = [
             "hotel",
         ],
         segments: [
+            // Hotel -> World Trade Center
             {mode: "subway"},
+            // World Trade Center -> Ground Zero
             {mode: "walk"},
+            // Ground Zero -> Oculus
             {mode: "walk"},
+            // Oculus -> Brooklyn Bridge
             {mode: "walk"},
+            // Brooklyn Bridge -> Westville DUMBO
             {mode: "walk"},
+            // Westville DUMBO -> Pebble Beach
             {mode: "walk"},
+            // Pebble Beach -> Statue of Liberty
             {
                 mode: "ferry",
                 via: [
@@ -74,13 +91,18 @@ export const days: Day[] = [
                     {x: 272, y: 960},
                 ],
             },
+            // Statue of Liberty -> USS Intrepid
             {
                 mode: "ferry",
                 via: [{x: 28, y: 870}],
             },
+            // USS Intrepid -> Nintendo Store
             {mode: "walk"},
+            // Nintendo Store -> Grand Central Terminal
             {mode: "walk"},
+            // Grand Central Terminal -> Grand Central Hot Dog Cart
             {mode: "walk"},
+            // Grand Central Hot Dog Cart -> Hotel
             {mode: "walk"},
         ],
     },
@@ -103,10 +125,21 @@ export const days: Day[] = [
             "hotel",
         ],
         segments: [
+            // Hotel -> Liberty Bagels
             {mode: "walk"},
-            {mode: "subway"},
+            // Liberty Bagels -> Central Park Zoo
+            {
+                mode: "subway",
+                via: [
+                    {x: 262, y: 544},
+                    {x: 262, y: 485},
+                ],
+            },
+            // Central Park Zoo -> Central Park
             {mode: "walk"},
+            // Central Park -> El Mitote
             {mode: "walk"},
+            // El Mitote -> Apple Fifth Avenue
             {
                 mode: "subway",
                 via: [
@@ -114,10 +147,22 @@ export const days: Day[] = [
                     {x: 122, y: 485},
                 ],
             },
+            // Apple Fifth Avenue -> Washington Square Park
             {mode: "subway"},
+            // Washington Square Park -> The Cage
             {mode: "walk"},
-            {mode: "subway"},
-            {mode: "subway"},
+            // The Cage -> 368 Broadway
+            {mode: "subway", via: [{x: 262, y: 706}]},
+            // 368 Broadway -> Burgerology
+            {
+                mode: "subway",
+                via: [
+                    {x: 262, y: 790},
+                    {x: 262, y: 686},
+                    {x: 262, y: 544},
+                ],
+            },
+            // Burgerology -> Hotel
             {mode: "walk"},
         ],
     },
@@ -144,19 +189,45 @@ export const days: Day[] = [
             "hotel",
         ],
         segments: [
+            // Hotel -> B&H Photo
             {mode: "walk"},
+            // B&H Photo -> Vessel
             {mode: "walk"},
+            // Vessel -> Hudson Yards
             {mode: "walk"},
+            // Hudson Yards -> High Line
             {mode: "walk"},
+            // High Line -> Chelsea Market
             {mode: "walk"},
+            // Chelsea Market -> Little Island
             {mode: "walk"},
+            // Little Island -> Chinatown
+            {
+                mode: "subway",
+                via: [
+                    {x: 126, y: 674},
+                    {x: 126, y: 764},
+                ],
+            },
+            // Chinatown -> New York Stock Exchange
             {mode: "subway"},
-            {mode: "subway"},
+            // New York Stock Exchange -> Wall Street Bull
             {mode: "walk"},
+            // Wall Street Bull -> Oculus
             {mode: "walk"},
-            {mode: "subway"},
+            // Oculus -> NY Pizza Suprema
+            {
+                mode: "subway",
+                via: [
+                    {x: 126, y: 764},
+                    {x: 126, y: 674},
+                ],
+            },
+            // NY Pizza Suprema -> Madison Square Garden
             {mode: "walk"},
+            // Madison Square Garden -> Penn Station
             {mode: "walk"},
+            // Penn Station -> Hotel
             {mode: "walk"},
         ],
     },
@@ -167,6 +238,9 @@ export const days: Day[] = [
         color: "var(--color-mta-brown)",
         travel: true,
         route: ["hotel", "lga"],
-        segments: [{mode: "car"}],
+        segments: [
+            // Hotel -> Airport
+            {mode: "car"},
+        ],
     },
 ]
